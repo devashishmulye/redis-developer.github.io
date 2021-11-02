97d28c25920f01863bd4888a8528bd63ac25c1b6
albin9747 <albin@finbox.in>
2021-11-02 23:16:12 +0530

    custom styles added

--- src/theme/RedisCard/index.jsx
+++ src/theme/RedisCard/index.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
-import {MDXProvider} from '@mdx-js/react';
-import MDXComponents from '@theme/MDXComponents';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import React from "react";
+import { MDXProvider } from "@mdx-js/react";
+import MDXComponents from "@theme/MDXComponents";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 import "./styles.css";
 
 const RoadmapItem = (props) => {
-    const [displayLong, setDisplayLong] = React.useState(false);
+  const [displayLong, setDisplayLong] = React.useState(false);
 
-    return <div className="ri-container">
-{props.img && <img src={props.img} style={{width:"30%"}}/>}
-        <div className="ri-description-short">
-            <div className="ri-icon"><span className="fe fe-zap" /></div>
-            
-            <div className="ri-detail">
-                <div className="ri-title"><a href={props.page}>{props.title}</a></div>
-                <div className="ri-description">
-                    {props.description}
-                    {React.Children.count(props.children) > 0 && <span className="ri-more fe fe-more-horizontal" onClick={() => setDisplayLong(!displayLong)}/>}
-
-                </div>
-            </div>
+  return (
+    <a href={props.page} className="ri-container">
+      {props.img && (
+        <div
+          style={{
+            width: "100%",
+            height: "55px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <img src={props.img} style={{ width: "40px" }} />
         </div>
-        {displayLong && <div className="ri-description-long"><MDXProvider components={MDXComponents}>{props.children}</MDXProvider></div>}
-    </div>
-}
+      )}
+      <div className="ri-description-short">
+        <div className="ri-icon">
+          <span className="fe fe-zap" />
+        </div>
+
+        <div className="ri-detail">
+          <div className="ri-title">
+            <a href={props.page}>{props.title}</a>
+          </div>
+          <div className="ri-description">
+            {props.description}
+            {React.Children.count(props.children) > 0 && (
+              <span
+                className="ri-more fe fe-more-horizontal"
+                onClick={() => setDisplayLong(!displayLong)}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      {displayLong && (
+        <div className="ri-description-long">
+          <MDXProvider components={MDXComponents}>{props.children}</MDXProvider>
+        </div>
+      )}
+    </a>
+  );
+};
 
 export default RoadmapItem;
